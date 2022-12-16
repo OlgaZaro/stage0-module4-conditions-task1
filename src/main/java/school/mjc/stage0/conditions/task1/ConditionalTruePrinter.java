be3c4a7fc5f0af9da66bb57fabef89aadf9cabd8
Olga Zarosylova <zoa1906@gmail.com>
2022-12-16 20:08:15 +0200

    //yes

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        int a = 5;
+        if (a == 5) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -1,6 +1,19 @@
 package school.mjc.stage0.conditions.task1;
 
 public class PositiveNumberDeterminer {
+    public static void main(String[] args) {
+
+
+        PositiveNumberDeterminer numberToBeDetermined = new PositiveNumberDeterminer();
+        numberToBeDetermined.isPositive(5);
+    }
+
     public void isPositive(int numberToBeDetermined) {
+
+        if (numberToBeDetermined > 0) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
     }
 }
